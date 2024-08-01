f13242502893369a22130b8583ecfbb675ab5732
tsars <tsarskiytsarskiy@mail/ru>
2024-08-01 15:10:05 +0700

    Не идет первая страница

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'post'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'post/list.html', {'posts': posts})
-
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 статьи на странице
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)  #сли страница не является целым числом возвращаем первую страницу
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        #Есло номенр страници больше чем страниц возвращаем последнюю страницу
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish_year=year, publish_month=month, publish_day=day)
-    return render(request, 'post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
+
